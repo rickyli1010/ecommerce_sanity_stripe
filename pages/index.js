3f07ece27096ac11694071c4cdb2badfed04f4b1
rickyli1010 <ricky_0321@hotmail.com>
2023-12-28 21:56:20 -0600

    footerbanner

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,11 @@ const Home = ({ products, bannerData }) => (
     </div>
     <div className="products-container">
       {products?.map((product) => (
-        <Product key={product.id} product={product} />
+        <Product key={product._id} product={product} />
       ))}
     </div>
-    <FooterBanner />
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
   </div>
 );
 
